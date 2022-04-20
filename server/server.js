517b94485b1d35a25e92bed2b28a8b2006bb8057
unknown <steinway.nathan@gmail.com>
2022-04-20 16:52:13 -0500

    added comments

--- server/server.js
+++ server/server.js
@@ -7,24 +7,26 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 const startServer = async () => {
-    // create a new Apollo server and pass in our schema data
+    // This imports our typeDefs and resolvers into the Apollo server
     const server = new ApolloServer({
         typeDefs,
         resolvers,
     });
 
-    // Start the Apollo server
+    // New Apollo requires an asynchronous server initialization to work
     await server.start();
 
-    // log where we can go to test our GQL API
+    // Playground url
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
+// Initializing the startServer function so it launches ApolloServer
 startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Opens the database only once
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
