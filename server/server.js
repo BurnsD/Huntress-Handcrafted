100a84ec108642286f2b76d2b9d42fda36db246c
unknown <steinway.nathan@gmail.com>
2022-04-20 16:48:46 -0500

    fixed bug

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 const startServer = async () => {
