a1cc49c176a7d82cb6bb2a1b7ac5ce7c7bb2fb10
unknown <steinway.nathan@gmail.com>
2022-04-20 16:45:56 -0500

    apollo server up and running, integrated with graphql!

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/huntress-handcrafted', {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-module.exports = mongoose.conection;
+module.exports = mongoose.connection;
