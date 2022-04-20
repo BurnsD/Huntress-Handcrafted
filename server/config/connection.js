0d02914a0f729d1c3d13f6493b906c6b00a1f293
unknown <steinway.nathan@gmail.com>
2022-04-20 16:55:31 -0500

    mystery commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// removed useFindAndModify because it's no longer supported
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/huntress-handcrafted', {
   useNewUrlParser: true,
   useUnifiedTopology: true
